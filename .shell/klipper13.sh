35e40934ca081ce0962f3665f1207254efac38f0
Sergei Rozhkov <zmod@zmod.link>
2026-09-05 21:35:33 +0500

    5.1.9

--- .shell/klipper13.sh
+++ .shell/klipper13.sh
@@ -24,8 +24,6 @@ if [ ${C5PRO} -eq 1 ] ||[ ${AD5X} -eq 1 ]; then
 fi
 if [ ${AD5M} -eq 1 ]; then
     find /opt/PROGRAM/control/ -name NationsCommand| while read a; do $a -r ; done;
-    [ -f /opt/config/mod_data/printer.cfg ] && cp /opt/config/mod_data/printer.cfg /opt/config/printer.cfg
-    [ -f /opt/config/mod_data/printer.base.cfg ] && cp /opt/config/mod_data/printer.base.cfg /opt/config/printer.base.cfg
 fi
 
 exit 0
